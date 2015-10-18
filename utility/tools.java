b87111b187c7ac901136dd8c6ef631fc392ad439
Zander <zackerman@mvgshome.org>
2015-10-18 19:53:10 -0400

    Messeging WIP
    Work in Progress
    
    TextInformBoxes are not working right...

--- utility/tools.java
+++ utility/tools.java
@@ -1,2 +1,16 @@
 package utility;
-//
+
+
+import net.slashie.libjcsi.textcomponents.TextInformBox;
+
+public class tools{
+	
+	public tools(){
+		
+	}
+	
+	public void sendMessage(TextInformBox messenger, String message){
+		messenger.addText(message+ " ");
+	}
+	
+}
